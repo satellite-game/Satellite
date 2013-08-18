c7d533a3340e8287c7347fb8e88508dde7345c3f
garytryan <garythomasryan@gmail.com>
2013-08-17 17:09:10 -0700

    initial position of turrets change with angle of the ship

--- client/game/classes/s.Turret.js
+++ client/game/classes/s.Turret.js
@@ -2,22 +2,29 @@ s.Turret = new Class({
 	extend: s.GameObject,
 
 	construct: function(options){
-	
 		this.root = new Physijs.SphereMesh(new THREE.SphereGeometry(10,20,20), new THREE.MeshNormalMaterial());
-		
+
 		this.root.position.copy(options.position);
 		this.root.rotation.copy(options.rotation);
 
-		this.root.position.x -= 500;
-		this.root.position.z -= 500;
+		// this.root.position.x -= 100;
+		// this.root.position.z -= 100;
 
-		this.update = this.update.bind(this);
-		this.game.hook(this.update);
+		// this.update = this.update.bind(this);
+		// this.game.hook(this.update);
 	},
 
-	update: function(delta, time){
-		this.root.position.x -= this.root.position.x *= 0.001;
-		this.root.position.z -= this.root.position.z *= 0.001;
+	xupdate: function(delta, time){
+		var root = this.thing;
+		var rotationMatrix = new THREE.Matrix4();
+		rotationMatrix.extractRotation(root.matrix);
+
+		var linearVelocity = root.getLinearVelocity().clone();
+		impulse = linearVelocity.clone().negate();
+		root.applyCentralImpulse(impulse);
+
+		var forceVector = new THREE.Vector3(0, 0, -100000).applyMatrix4(rotationMatrix);
+		root.applyCentralImpulse(forceVector);
 	}
 
 });
