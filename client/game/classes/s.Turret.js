2d64f9664f5836123084f615a79a06e1ea8fb5bc
garytryan <garythomasryan@gmail.com>
2013-08-20 14:58:05 -0700

    Turrets refactored to use a Projectile parent class

--- client/game/classes/s.Turret.js
+++ client/game/classes/s.Turret.js
@@ -1,36 +1,17 @@
 s.Turret = new Class({
-	extend: s.GameObject,
+	extend: s.Projectile,
 
 	construct: function(options){
-        this.color = s.config.weapons.bullets.color[options.team];
-        this.velocity = s.config.weapons.bullets.velocity;
-        var material = Physijs.createMaterial( new THREE.MeshBasicMaterial({color: this.color}), 0.8, 0.3);
-		this.root = new Physijs.SphereMesh(new THREE.SphereGeometry(3,4,4), material, 1);
-        this.root.instance = this;
-		this.root.position.copy(options.position);
-		this.root.rotation.copy(options.rotation);
-        this.initialVelocity = options.initialVelocity;
-	},
+        // Add a collision mesh
+        this.addCollisionMesh(new THREE.SphereGeometry(60));
 
-	init: function(_super){
-        _super.call(this);
+        // Draw the projectile to the screen
+        this.geometry = new THREE.SphereGeometry(12,20,20);
+        this.material = new THREE.MeshBasicMaterial({color: 'yellow'});
+        this.root.add(new THREE.Mesh(this.geometry, this.material, 0.1));
 
-        // Make sure the bullets matrix is up to date
-        this.root.updateMatrix();
-
-        // Extract the rotation from the bullets matrix
-		this.rotationMatrix = new THREE.Matrix4();
-		this.rotationMatrix.extractRotation(this.root.matrix);
-
-        // Apply bullet impulse
-
-        this.forceVector = new THREE.Vector3(0, 0, (this.velocity * -1) + (this.initialVelocity.z > 0 ? this.initialVelocity.z * -1 : this.initialVelocity.z )).applyMatrix4(this.rotationMatrix);
-		this.root.applyCentralImpulse(this.forceVector);
-        this.root.addEventListener('collision', this.handleCollision.bind(this));
-    },
-
-    handleCollision: function(){
-        this.destruct();
-    }
-
+        // Position the projectile relative to the ship
+        this.root.position.copy(options.position);
+        this.root.rotation.copy(options.rotation);
+	}
 });
