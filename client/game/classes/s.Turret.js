4c5f2c6428d25a047c560f8a807a140652f0fcb7
garytryan <garythomasryan@gmail.com>
2013-08-27 18:20:54 -0700

    Demo config pt:2

--- client/game/classes/s.Turret.js
+++ client/game/classes/s.Turret.js
@@ -5,7 +5,7 @@ s.Turret = new Class({
 
     options: {
         mass: 1,
-        size: 16,
+        size: 25,
         velocity: 10000,
         damage: 1000,
         scale: new THREE.Vector3(50, 50, 1.0),
