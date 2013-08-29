8371325e4ddcf0c4fbb7cd8240194565992fe072
garytryan <garythomasryan@gmail.com>
2013-08-29 10:48:33 -0700

    Removed some css from load screen

--- client/game/classes/s.LoadScreen.js
+++ client/game/classes/s.LoadScreen.js
@@ -8,15 +8,7 @@ s.LoadScreen = new Class( {
 							'<h1>loading...</h1>'+
 						'</section>' +
 					'</div>');
-        this.el.css({
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            zIndex: 101,
-            backgroundColor: 'black'
-        }).appendTo('body');
+        this.el.appendTo('body');
 	},
 
 	remove: function(){
