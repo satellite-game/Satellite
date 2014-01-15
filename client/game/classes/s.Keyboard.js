3e4f5e199fa892dc786818a866ca4454fb87e0a8
ehannum <ehannum@gmail.com>
2014-01-15 11:23:16 -0800

    oculus now 1:1 controls pitch, yaw and roll of ship in game. fixes #123 fixes #124.

--- client/game/classes/s.Keyboard.js
+++ client/game/classes/s.Keyboard.js
@@ -1,68 +1,69 @@
 s.Keyboard = new Class({
-	toString: 'Keyboard',
+  toString: 'Keyboard',
 
-	keys: {
-		'left'		: 37,
-		'up'		: 38,
-		'right'		: 39,
-		'down'		: 40,
-		'space'		: 32,
-		'pageup'	: 33,
-		'pagedown'	: 34,
-		'tab'		: 9,
-		'w'			: 87,
-		'a'			: 65,
-		's'			: 83,
-		'd'			: 68,
-		'backtick'	: 192,
-		'shift'		: 16
-	},
+  keys: {
+    'left'    : 37,
+    'up'      : 38,
+    'right'   : 39,
+    'down'    : 40,
+    'space'   : 32,
+    'pageup'  : 33,
+    'pagedown': 34,
+    'tab'     : 9,
+    'w'       : 87,
+    'a'       : 65,
+    's'       : 83,
+    'd'       : 68,
+    'backtick': 192,
+    'shift'   : 16,
+    'tilde'   : 192
+  },
 
-	modifiers: ['shift', 'ctrl', 'alt', 'meta'],
+  modifiers: ['shift', 'ctrl', 'alt', 'meta'],
 
-	construct: function(game, player) {
-		this.keyCodes = {};
+  construct: function(game, player) {
+    this.keyCodes = {};
 
-		// Listen to key events
-		window.addEventListener('keydown', this.handleKeyChange.bind(this, true), false);
-		window.addEventListener('keyup', this.handleKeyChange.bind(this, false), false);
-	},
+    // Listen to key events
+    window.addEventListener('keydown', this.handleKeyChange.bind(this, true), false);
+    window.addEventListener('keyup', this.handleKeyChange.bind(this, false), false);
+  },
 
-	destruct: function() {
-		window.removeEventListener('keydown', this.handleKeyChange, false);
-		window.removeEventListener('keyup', this.handleKeyChange, false);
-	},
+  destruct: function() {
+    window.removeEventListener('keydown', this.handleKeyChange, false);
+    window.removeEventListener('keyup', this.handleKeyChange, false);
+  },
 
-	handleKeyChange: function(pressed, e) {
-		var keyCode	= e.keyCode;
-		this.keyCodes[keyCode] = pressed;
+  handleKeyChange: function(pressed, e) {
+    var keyCode = e.keyCode;
+    this.keyCodes[keyCode] = pressed;
 
-		// update this.modifiers
-		this.modifiers.shift = e.shiftKey;
-		this.modifiers.ctrl = e.ctrlKey;
-		this.modifiers.alt = e.altKey;
-		this.modifiers.meta = e.metaKey;
-	},
+    // update this.modifiers
+    this.modifiers.shift = e.shiftKey;
+    this.modifiers.ctrl = e.ctrlKey;
+    this.modifiers.alt = e.altKey;
+    this.modifiers.meta = e.metaKey;
+  },
 
-	pressed: function(keyDesc) {
-		var keys = keyDesc.split("+");
-		for (var i = 0; i < keys.length; i++) {
-			var key	= keys[i];
-			var pressed;
-			if (this.modifiers.indexOf(key) !== -1) {
-				pressed	= this.modifiers[key];
-			}
-			else if (Object.keys(this.keys).indexOf(key) != -1) {
-				pressed	= this.keyCodes[this.keys[key]];
-			}
-			else {
-				pressed	= this.keyCodes[key.toUpperCase().charCodeAt(0)];
-			}
+  pressed: function(keyDesc) {
+    var keys = keyDesc.split("+");
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var pressed;
+      if (this.modifiers.indexOf(key) !== -1) {
+        pressed = this.modifiers[key];
+      }
+      else if (Object.keys(this.keys).indexOf(key) != -1) {
+        pressed = this.keyCodes[this.keys[key]];
+      }
+      else {
+        pressed = this.keyCodes[key.toUpperCase().charCodeAt(0)];
+      }
 
-			if(!pressed) return false;
-		}
+      if(!pressed) return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 });
 
