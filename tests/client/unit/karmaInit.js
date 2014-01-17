6eca79f791a42b02298b132dc53f4d419c73a1be
Sam Stites <stites.sr@gmail.com>
2014-01-16 20:44:28 -0800

    karma/mocha/chai/sinon working client-side and server side with grunt

--- tests/client/unit/karmaInit.js
+++ tests/client/unit/karmaInit.js
@@ -8,7 +8,7 @@ describe('karma tests with chai', function(){
       expect('foo').to.not.equal('bar');
   });
 
-  it('should expose the Chai `should` property', function(){
+  it('should expose the Chai should property', function(){
       (1).should.not.equal(2);
       should.exist(123);
   });
