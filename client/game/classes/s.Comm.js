70f821b77f8ebe557816f10679fa136465dd3d2e
steinernein <steinernein@gmail.com>
2014-01-17 04:23:28 -0800

    Starting integration - late

--- client/game/classes/s.Comm.js
+++ client/game/classes/s.Comm.js
@@ -108,6 +108,7 @@ s.Comm = new Class( {
         var shipPosition = this.game.player.getPositionPacket( );
 
         var packet = {
+            room: this.room,
 
             evt: 'joined',
 
@@ -121,7 +122,7 @@ s.Comm = new Class( {
 
             aVeloc: shipPosition.aVeloc,
 
-            lVeloc: shipPosition.lVeloc
+            lVeloc: shipPosition.lVeloc,
 
         };
 
