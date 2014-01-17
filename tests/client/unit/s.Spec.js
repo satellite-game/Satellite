01a01ee61299410cdd454dd8505e4ccb2acdd29c
Joao Stein <joaosteing@gmail.com>
2014-01-17 03:05:38 -0800

    Refactor tests

--- tests/client/unit/s.Spec.js
+++ tests/client/unit/s.Spec.js
@@ -6,15 +6,16 @@ describe('Satellite game', function () {
   });
 
   it('should contains ship properties', function () {
+    expect(s.config.ship).to.be.an('object');
     expect(s).to.have.deep.property('config.ship.hull');
     expect(s).to.have.deep.property('config.ship.shields');
     expect(s).to.have.deep.property('config.ship.maxSpeed');
   });
 
   it('should init the game', function () {
-    var spy = sinon.spy();
-    s.init('init', spy);
-    spy.called.should.equal.true;
+    var spy = sinon.spy(s, 'init');
+    s.init();
+    expect(spy.called).to.equal(true);
 
     expect(s).to.have.property('projector').and.to.be.an('object');
     expect(s).to.have.property('loader').and.to.be.an('object');
