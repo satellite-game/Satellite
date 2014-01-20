d911efa54af37c3af87e820441b79ce96e00205b
ehannum <ehannum@gmail.com>
2014-01-20 14:33:39 -0800

    Relocated player construction to s.Player to handle lighting and engine trail

--- client/game/classes/s.Player.js
+++ client/game/classes/s.Player.js
@@ -1,13 +1,41 @@
 s.Player = new Class({
-	extend: s.Ship,
-	construct: function(options) {
-		// Nothing here yet...
-		// Maybe collision detection and whatnot
-	this.game.hook(this.update);
-	},
-	update: function() {
-		if (this.hull <= 0){
-			this.game.handleDie();
-		}
-	}
+  extend: s.Ship,
+  construct: function(options) {
+
+    this.camera = options.camera;
+    this.game = options.game;
+    this.HUD = options.HUD;
+    this.firstPerson = false;
+
+    this.root.castShadow = true;
+
+    // Moon facing initilization
+    //this.player.root.lookAt(this.moon.root.position);
+
+    // Root camera to the player's position
+    this.root.add( this.camera );
+
+    this.trailGlow = new THREE.PointLight(0x00FFFF, 5, 20);
+
+    // Adjust this between 0 and 30 based engine thrust.
+    // Static 30.0 for testing.
+    this.trailGlow.intensity = 30.0;
+    this.root.add( this.trailGlow );
+    this.trailGlow.position.set(0, 0, 35);
+
+    // this.player.root.add( new THREE.PointLightHelper(this.trailGlow, 1) );
+    this.game.hook(this.update);
+
+    // Setup camera: Cockpit view; COMMENT OUT FOR CHASE CAM
+    // this.camera.position.set( 0, 0, 0 );
+
+    // Setup camera: Chase view
+    this.game.camera.position.set(0,35,250);
+  },
+  update: function() {
+    if (this.hull <= 0){
+      this.game.handleDie();
+    }
+  }
+
 });
