77ec467e57355805cddc4548df9a5ce9415f912f
ehannum <ehannum@gmail.com>
2014-01-20 15:56:46 -0800

    Player engine glow now working based on linear velocity

--- client/game/classes/s.Player.js
+++ client/game/classes/s.Player.js
@@ -19,7 +19,7 @@ s.Player = new Class({
 
     // Adjust this between 0 and 30 based engine thrust.
     // Static 30.0 for testing.
-    this.trailGlow.intensity = 30.0;
+    this.trailGlow.intensity = 0;
     this.root.add( this.trailGlow );
     this.trailGlow.position.set(0, 0, 35);
 
@@ -36,6 +36,8 @@ s.Player = new Class({
     if (this.hull <= 0){
       this.game.handleDie();
     }
+    this.trailGlow.intensity = Math.sqrt(~~Math.abs(this.root.getLinearVelocity().x * this.root.getLinearVelocity().x) + ~~Math.abs(this.root.getLinearVelocity().y * this.root.getLinearVelocity().y) + ~~Math.abs(this.root.getLinearVelocity().z * this.root.getLinearVelocity().z))/100;
+    console.log(this.trailGlow.intensity);
   }
 
 });
