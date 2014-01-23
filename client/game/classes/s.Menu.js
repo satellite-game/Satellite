b075f5154d832a086c0fba2cf0ce80deaae29c97
ehannum <ehannum@gmail.com>
2014-01-22 21:25:59 -0800

    [wip] loads of menu modularity work, mostly for scalable room list sizes.

--- client/game/classes/s.Menu.js
+++ client/game/classes/s.Menu.js
@@ -11,15 +11,15 @@ s.Menu = new Class({
     this.HUD = options.game.HUD;
     this.oculus = options.game.oculus;
     this.menuItems = [];
+    this.menuScreen = 'default';
 
     // PlaneGeometry would be better than a cube for this but harder to write because 
     // it would need to be rotated and all it's child objects would need to be rotated back.
     // Optimize at your own risk.
     this.menuBox = new THREE.Mesh( new THREE.CubeGeometry(2500, 2500, 1), new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, opacity: 0.7}) );
 
-    // Adding menu item format: {text: 'text_string', font: 'font_name_string(optional)'};
-    this.addMenuItems([{text: 'JOIN GAME'}, {text: 'DISCONECT'}, {text: 'LEADERBOARD'}]);
-
+    // Adding menu item format: {text: 'displayed_text_string'(required), font: 'font_name_string'(default "helvetiker"), bold: true/false (default false), size: number(1-5)(defualt 3), flat: true/false(changes shader, depth and bevel)(default false), small: true/false(overides flat, bold, and size to make a default readable small font)(default false)};
+    
     this.menuBox.position.setZ(-150);
     this.menuBox.visible = false;
 
@@ -28,18 +28,55 @@ s.Menu = new Class({
     if (this.oculus.detected) {
       this.menuBox.position.setZ(-50);
     }
+
+    this.showDefaultMenu();
   },
   addMenuItems: function ( items ) {
     // procedurally center aligns text, vertically center aligns menu
-    var menuHeight = items.length * 10;
+    var menuHeight = 0;
+    for (var j = 0; j < items.length; j++) {
+      if (items[j].small) {
+        menuHeight += 4;
+      } else {
+        var itemSize = items[j].size || 3;
+        menuHeight += itemSize*2;
+      }
+    }
     for (var i = 0; i < items.length; i++) {
       // this is currently the only font and it's only in bold
       // google typeface.js for how to add more.
-      var font = items[items.length-i-1].font || 'helvetiker';
-      var menuItemGeo = new THREE.TextGeometry(items[items.length-i-1].text, {font: font, size: 5, height: 5, weight: 'bold', bevelEnabled: true, bevelThickness: 0.3, bevelSize: 0.3});
-      var menuItemMaterial = new THREE.MeshPhongMaterial({color: 0x00FF00});
+      var bevelEnabled,
+          bevel,
+          mat,
+          bold,
+          size,
+          font = items[items.length-i-1].font || 'helvetiker';
+
+      if (items[items.length-i-1].small) {
+        size = 2;
+        bold = 'normal';
+        bevelEnabled = false;
+        bevel = 0;
+        mat = 'MeshBasicMaterial';
+      } else {
+        size = items[items.length-i-1].size || 3;
+        bold = items[items.length-i-1].bold ? 'bold' : 'normal';
+
+        if (items[items.length-i-1].flat) {
+          bevelEnabled = false;
+          bevel = 0;
+          mat = 'MeshBasicMaterial';
+        } else {
+          bevelEnabled = true;
+          bevel = 0.2;
+          mat = 'MeshPhongMaterial';
+        }
+      }
+
+      var menuItemGeo = new THREE.TextGeometry(items[items.length-i-1].text, {font: font, size: size, height: size*2, weight: bold, bevelEnabled: bevelEnabled, bevelThickness: bevel, bevelSize: bevel});
+      var menuItemMaterial = new THREE[mat]({color: 0x00FF00});
       var menuItem = new THREE.Mesh(menuItemGeo, menuItemMaterial);
-      menuItem.position.setY((10*i)-(menuHeight/2)+2.5); // MATH!
+      menuItem.position.setY((i*(size*2))-(menuHeight/2)+(size/2)); // MATH!
       menuItem.position.setX(menuItem.geometry.boundingSphere.radius*-0.5);
       menuItem.visible = false;
       this.menuBox.add( menuItem );
@@ -49,9 +86,12 @@ s.Menu = new Class({
     // I'm really surprised it's this easy.
     // I wonder if the menu items are even being garbage collected.
     // I mean, I ASSUME they are...
-    this.menuBox.children = [];
+    // this.menuBox.children = [];
   },
   open: function () {
+    if (this.menuScreen !== 'default') {
+      this.showDefaultMenu();
+    }
     this.displayed = true;
     this.menuBox.visible = true;
     for (var i = 0; i < this.menuBox.children.length; i++) {
@@ -64,10 +104,28 @@ s.Menu = new Class({
     for (var i = 0; i < this.menuBox.children.length; i++) {
       this.menuBox.children[i].visible = false;
     }
+    if (this.menuScreen !== 'default') {
+      this.clearMenu();
+    }
   },
   updateSelection: function () {
     if (this.displayed) {
       // todo: the part where you interact with the menu at all.
     }
+  },
+  showDefaultMenu: function () {
+    this.menuScreen = 'default';
+    this.addMenuItems([{text: 'JOIN GAME', size: 5}, {text: 'DISCONECT', size: 5}, {text: 'LEADERBOARD', size: 5}, {text: 'SMALL TEST TEXT 1', small: true}, {text: 'SMALL TEST TEXT 2', small: true}, {text: 'SMALL TEST TEXT 3', small: true}]);
+  },
+  showRoomList: function () {
+    this.clearMenu();
+    this.menuScreen = 'rooms';
+    var roomNames = [];
+    // some database stuff to get open rooms
+    var rooms = [];
+    for (var i = 0; i < roomNames.length; i++) {
+      rooms.push({text: roomNames[i], small: true});
+    }
+    this.addMenuItems(rooms);
   }
 });
