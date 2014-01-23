e34e2285a996ecc30e1a3a512aedee323b1c1410
Sam Stites <stites.sr@gmail.com>
2014-01-22 21:53:42 -0800

    [WIP] logging and added kill emissions

--- server/events/player.js
+++ server/events/player.js
@@ -2,6 +2,10 @@ module.exports = function (host, sync) {
   return {
 
     killed: function (socket, packet) {
+      socket.emit('killed', {
+        killed: packet.you,
+        killer: packet.killer
+      });
       socket.broadcast.emit('killed', {
         killed: packet.you,
         killer: packet.killer
--- server/io/manager.js
+++ server/io/manager.js
@@ -8,26 +8,32 @@ var Manager = function( context, io ) {
   return function( socket ) {
 
     socket.on('join', function( args ) {
+      console.log('join');
       events.flow['join'](socket, args);
     });
 
     socket.on('player', function( type, args ) {
+      console.log('player');
       events.player[type](socket, args);
     });
 
     socket.on('bot', function( type, args ) {
+      console.log('bot');
       events.bot[type](socket, args);
     });
 
     socket.on('combat', function( type, args ) {
+      console.log('combat');
       events.combat[type](socket, args);
     });
 
     socket.on('disconnect', function( args ) {
+      console.log('disconnect');
       events.flow['disconnect'](socket, args);
     });
 
     socket.on('keypress', function( type, args) {
+      console.log('keypress');
       events.movement.keySync( socket, args );
     });
   };
