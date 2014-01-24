39ef9bffdd9519e7593bd9e89eb379f8072afe55
Joao Stein <joaosteing@gmail.com>
2014-01-24 14:32:49 -0800

    Add new bot instance before each test

--- tests/client/unit/s.BotSpec.js
+++ tests/client/unit/s.BotSpec.js
@@ -1,5 +1,18 @@
 describe('Bot class', function () {
-  it('', function () {
 
+  beforeEach(function () {
+    s.init();
+    var bot = new s.Bot({
+      game: s.game,
+      name: 'bot 1',
+      shipClass: 'human_ship_heavy',
+      position: [22498, -25902, 24976],
+      rotation: [0, Math.PI / 2, 0],
+      alliance: 'enemy'
+    });
+  });
+
+  it('should create new Bot instance', function () {
+    expect(bot).to.be.an('object');
   });
 });
