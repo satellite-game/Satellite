9f15f6881a9dd0d149d3e04e2a876eea2f16428c
Joao Stein <joaosteing@gmail.com>
2014-01-24 21:59:44 -0800

    add this.botCount to the constructor function of satellite game

--- tests/client/unit/s.BotSpec.js
+++ tests/client/unit/s.BotSpec.js
@@ -1,18 +1,34 @@
 describe('Bot class', function () {
-  it('should create new Bot instance', function (cb) {
-      setTimeout(function () {
-        var bot = new s.Bot({
-          game: s.game,
-          name: 'bot 1',
-          shipClass: 'human_ship_heavy',
-          position: [22498, -25902, 24976],
-          rotation: [0, Math.PI / 2, 0],
-          alliance: 'enemy'
-        });
+  it('should create new Bot instance', function (done) {
+    setTimeout(function () {
+      var bot = new s.Bot({
+        game: s.game,
+        name: 'bot 1',
+        shipClass: 'human_ship_heavy',
+        position: [22498, -25902, 24976],
+        rotation: [0, Math.PI / 2, 0],
+        alliance: 'enemy'
+      });
 
-        expect(bot).to.be.an('object');
+      expect(bot).to.be.an('object');
+      expect(bot.isBot).to.equal(true);
 
-        cb();
-      }, 1000);
+      done();
+    }, 1000);
+  });
+
+  it('should increment bot counter', function (done) {
+    setTimeout(function () {
+      var bot = new s.Bot({
+        game: s.game,
+        shipClass: 'human_ship_heavy',
+        position: [22498, -25902, 24976],
+        rotation: [0, Math.PI / 2, 0],
+        alliance: 'enemy'
+      });
+      expect(s.game.botCount).to.equal(1);
+      done();
+    }, 1000);
+
   });
 });
