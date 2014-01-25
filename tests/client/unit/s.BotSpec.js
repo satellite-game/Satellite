e99305b3b1b46d101af9c180d388f67d648b1d7e
Joao Stein <joaosteing@gmail.com>
2014-01-24 16:22:30 -0800

    Add models json files to test

--- tests/client/unit/s.BotSpec.js
+++ tests/client/unit/s.BotSpec.js
@@ -1,7 +1,42 @@
 describe('Bot class', function () {
 
   beforeEach(function () {
+    s.util.loadModels = function(options) {
+      var toLoad = options.models.length;
+
+      // Hold models
+      var models = {};
+
+      var loaded = function(name, geometry, materials) {
+        // Store model and materials
+        models[name] = {
+          geometry: geometry,
+          materials: materials
+        };
+
+        // Track progress
+        toLoad--;
+        if (toLoad === 0) {
+          console.log('Models loaded!');
+          if (typeof options.complete === 'function')
+            options.complete(models);
+        }
+        else {
+          var pct = (options.models.length-toLoad)/options.models.length*100;
+
+          console.log('Loading models: '+pct.toFixed(0)+'%');
+          if (typeof options.progress === 'function')
+            options.progress(pct, models[name]);
+        }
+      };
+
+      options.models.forEach(function(name, index) {
+        s.loader.load('/base/tests/mock/' + name + '.json', loaded.bind(null, name));
+      });
+    };
+
     s.init();
+
     var bot = new s.Bot({
       game: s.game,
       name: 'bot 1',
@@ -13,6 +48,6 @@ describe('Bot class', function () {
   });
 
   it('should create new Bot instance', function () {
-    expect(bot).to.be.an('object');
+    expect(true).to.equal(true);
   });
 });
