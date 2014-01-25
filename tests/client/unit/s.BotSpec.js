c4f13c5150b1e9db0acf601e0d86d19760ad5110
Joao Stein <joaosteing@gmail.com>
2014-01-24 22:48:28 -0800

    abstract setTimeout logic to a function that create new bot and run the expects statements after loading all game models

--- tests/client/unit/s.BotSpec.js
+++ tests/client/unit/s.BotSpec.js
@@ -1,34 +1,36 @@
 describe('Bot class', function () {
-  it('should create new Bot instance', function (done) {
+
+  var makeBot = function (specs, done) {
     setTimeout(function () {
       var bot = new s.Bot({
         game: s.game,
-        name: 'bot 1',
         shipClass: 'human_ship_heavy',
         position: [22498, -25902, 24976],
         rotation: [0, Math.PI / 2, 0],
         alliance: 'enemy'
       });
 
-      expect(bot).to.be.an('object');
-      expect(bot.isBot).to.equal(true);
-
+      specs(bot);
       done();
     }, 1000);
+  };
+
+  it('should create new Bot instance', function (done) {
+    var specs = function (bot) {
+      expect(bot).to.be.an('object');
+      expect(bot.isBot).to.equal(true);
+    };
+
+    makeBot(specs, done);
   });
 
   it('should increment bot counter', function (done) {
-    setTimeout(function () {
-      var bot = new s.Bot({
-        game: s.game,
-        shipClass: 'human_ship_heavy',
-        position: [22498, -25902, 24976],
-        rotation: [0, Math.PI / 2, 0],
-        alliance: 'enemy'
-      });
-      expect(s.game.botCount).to.equal(1);
-      done();
-    }, 1000);
+    var specs = function (bot) {
+      // Expect 2 because the spec share the same game object
+      // Previous test add the first one
+      expect(s.game.botCount).to.equal(2);
+    };
 
+    makeBot(specs, done);
   });
 });
