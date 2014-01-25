25e763bcb9b9b3883ef3fdf843f201c1c66a8d1d
Brandon Cooper <cooperjbrandon@gmail.com>
2014-01-24 17:39:42 -0800

    respawn works for multiple players

--- client/game/classes/s.SatelliteGame.js
+++ client/game/classes/s.SatelliteGame.js
@@ -472,6 +472,7 @@ s.SatelliteGame = new Class( {
             that.player.shields = s.config.ship.shields;
             that.player.hull = s.config.ship.hull;
             that.player.setPosition([that.getRandomCoordinate(), that.getRandomCoordinate(), that.getRandomCoordinate()],[0,0,0],[0,0,0],[0,0,0]);
+            that.hostPlayer = false;
             that.restart();
         }, 6000);
     },
