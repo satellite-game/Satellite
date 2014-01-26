74ebb51f34e3b51f182087745e65b84a29be51f7
Brandon Cooper <cooperjbrandon@gmail.com>
2014-01-25 18:25:50 -0800

    commit build

--- build/client/game/s.js
+++ build/client/game/s.js
@@ -51440,6 +51440,64 @@ s.Moon = new Class({
 	}
 });
 
+s.BuildingTall = new Class({
+	extend: s.GameObject,
+
+	construct: function(options){
+		// handle parameters
+		this.options = options = jQuery.extend({
+			position: options.position,
+			rotation: options.rotation
+		}, options);
+
+		var geometry = s.models.human_building_tall.geometry;
+		var materials = s.models.human_building_tall.materials;
+
+		// Setup physical properties
+		materials[0] = Physijs.createMaterial(
+			materials[0],
+			1, // high friction
+			0.4 // low restitution
+		);
+
+		this.root = new Physijs.ConvexMesh(geometry, new THREE.MeshFaceMaterial(materials), 0);
+
+        this.root.name = "building_tall";
+		this.root.position.copy(options.position);
+		this.root.rotation.copy(options.rotation);
+		// this.root.receiveShadow = true; // Causes shader error
+	}
+});
+
+s.BuildingShort = new Class({
+	extend: s.GameObject,
+
+	construct: function(options){
+		// handle parameters
+		this.options = options = jQuery.extend({
+			position: options.position,
+			rotation: options.rotation
+		}, options);
+
+		var geometry = s.models.human_building_short.geometry;
+		var materials = s.models.human_building_short.materials;
+
+		// Setup physical properties
+		materials[0] = Physijs.createMaterial(
+			materials[0],
+			1, // high friction
+			0.4 // low restitution
+		);
+
+		this.root = new Physijs.ConvexMesh(geometry, new THREE.MeshFaceMaterial(materials), 0);
+
+        this.root.name = "building_short";
+		this.root.position.copy(options.position);
+		this.root.rotation.copy(options.rotation);
+		// this.root.receiveShadow = true; // Causes shader error
+	}
+});
+
 s.SpaceStation = new Class({
 	extend: s.GameObject,
 
@@ -51462,7 +51520,7 @@ s.SpaceStation = new Class({
 
 		this.root = new Physijs.ConvexMesh(geometry, new THREE.MeshFaceMaterial(materials), 0);
 
-        this.root.name = "space_station";
+    this.root.name = "space_station";
 		this.root.position.copy(options.position);
 		this.root.rotation.copy(options.rotation);
 		// this.root.receiveShadow = true; // Causes shader error
@@ -53300,7 +53358,7 @@ s.Menu = new Class({
     this.camera.add( this.menuBox );
 
     if (this.oculus.detected) {
-      this.menuBox.position.setZ(-150);
+      this.menuBox.position.setZ(-50);
     }
   },
 
@@ -53872,7 +53930,9 @@ s.SatelliteGame = new Class( {
 		'phobos_lofi',
         'human_ship_heavy',
 		'human_ship_light',
-        'human_space_station'
+        'human_space_station',
+        'human_building_short',
+        'human_building_tall'
 	],
 
     textures: [
@@ -53922,6 +53982,13 @@ s.SatelliteGame = new Class( {
             game: this
         } );
 
+        // Random building
+        this.building = new s.BuildingTall({
+            game: this,
+            position: new THREE.Vector3(-5211.99169921875, -1277.7318115234375, 3610.850830078125),
+            rotation: new THREE.Vector3(1.6990602929726026, 0.011873913392131176, 0.86412056066792210)
+        });
+
         this.pilot = {};
         this.callsigns = this.callsigns || ["Apollo","Strobe","Sage","Polkadot","Moonglow","Steel","Vanguard","Prong","Uptight","Blackpony","Hawk","Ramrod","Dice","Falcon","Rap","Buckshot","Cobra","Magpie","Warhawk","Boxer","Devil","Hammer","Phantom","Sharkbait","Dusty","Icon","Blade","Pedro","Stinger","Yellow Jacket","Limit","Sabre","Misty","Whiskey","Dice","Antic","Arrow","Auto","Avalon","Bandit","Banshee","Blackjack","Bulldog","Caesar","Cajun","Challenger","Chuggs","Cindy","Cracker","Dagger","Dino","Esso","Express","Fangs","Fighting Freddie","Freight Train","Freemason","Fury","Gamma","Gear","Ghost","Ginger","Greasy","Havoc","Hornet","Husky","Jackal","Jaguar","Jedi","Jazz","Jester","Knife","Kitty Hawk","Knight","Knightrider","Koala","Komono","Lancer","Lexus","Lion","Levi","Lucid","Malty","Mail Truck","Magma","Magnet","Malibu","Medusa","Maul","Monster","Misfit","Moss","Moose","Mustang","Nail","Nasa","Nacho","Nighthawk","Ninja","Neptune","Odin","Occult","Nukem","Ozark","Pagan","Pageboy","Panther","Peachtree","Phenom","Polestar","Punisher","Ram","Rambo","Raider","Raven","Razor","Rupee","Sabre","Rust","Ruin","Sultan","Savor","Scandal","Scorpion","Shooter","Smokey","Sniper","Spartan","Thunder","Titus","Titan","Timber Wolf","Totem","Trump","Venom","Veil","Viper","Weasel","Warthog","Winter","Wiki","Wild","Yonder","Yogi","Yucca","Zeppelin","Zeus","Zesty"];
 
@@ -53932,18 +53999,18 @@ s.SatelliteGame = new Class( {
             game: this
         } );
 
+        if (this.oculus.detected) {
+            console.log('Activating oculus HUD');
+            this.HUD.canvas.style.display = 'none';
+            this.HUD.oculusCanvas.style.display = 'block';
+        }
+
         // Add menu
         this.menu = new s.Menu({
             game: this
         });
 
         this.menu.showInitialMenu();
-
-        if (this.oculus.detected) {
-            console.log('Activating oculus HUD');
-            this.HUD.canvas.style.display = 'none';
-            this.HUD.oculusCanvas.style.display = 'block';
-        }
 
         this.player = new s.Player( {
             HUD: this.HUD,
@@ -54322,23 +54389,9 @@ s.SatelliteGame = new Class( {
         if (!you) {
             return;
         }
-        if (this.hostPlayer) { clearInterval(this.botPositionInterval); }
-<<<<<<< HEAD
-        // var HUD = s.game.HUD;
-        // HUD.ctx.fillStyle = "rgba(0,0,0,0.5)";
-        // HUD.ctx.fillRect(0,0,HUD.canvas.width,HUD.canvas.height);
-        // HUD.ctx.drawImage(HUD.gameOver,HUD.canvas.width/2 - HUD.gameOver.width/2,HUD.canvas.height/2 - HUD.gameOver.height/2);
+        if (this.hostPlayer) clearInterval(this.botPositionInterval);
         this.menu.gameOver(killer);
         if (s.game.roomSelected) s.game.comm.died(you, killer);
-
-        // s.game.stop();
-=======
-        s.game.stop();
-        var HUD = s.game.HUD;
-        HUD.ctx.fillStyle = "rgba(0,0,0,0.5)";
-        HUD.ctx.fillRect(0,0,HUD.canvas.width,HUD.canvas.height);
-        HUD.ctx.drawImage(HUD.gameOver,HUD.canvas.width/2 - HUD.gameOver.width/2,HUD.canvas.height/2 - HUD.gameOver.height/2);
-        s.game.comm.died(you, killer);
 
         this.restartGame();
     },
@@ -54352,7 +54405,6 @@ s.SatelliteGame = new Class( {
             that.hostPlayer = false;
             that.restart();
         }, 6000);
->>>>>>> d2ebd56795423c8d2a3efa05952771eda1a41a0e
     },
 
     shieldBoost: function(){
