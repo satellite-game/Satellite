4dc36c6446203fe2b1d31268c2ae0db676357fea
Joao Stein <joaosteing@gmail.com>
2014-01-25 17:17:03 -0800

    Update s.util.loadModels load path and include s.util.loadTextures function

--- tests/client/preload.js
+++ tests/client/preload.js
@@ -33,9 +33,49 @@ s.util.loadModels = function(options) {
   };
 
   options.models.forEach(function(name, index) {
-    s.loader.load('/base/tests/mock/' + name + '.json', loaded.bind(null, name));
+    s.loader.load('/base/tests/mock/models/' + name + '.json', loaded.bind(null, name));
   });
 };
+
+/*
+s.util.loadTextures is declared again to override new THREE.ImageUtils.loadTexture()
+path to use '/base/tests/mock' making it work with karma.
+*/
+
+s.util.loadTextures = function(options) {
+  var toLoad = options.textures.length;
+
+  // Hold textures
+  var textures = {};
+
+  var loaded = function(name, texture) {
+    // Store texture
+    textures[name] = texture;
+
+    // Track progress
+    toLoad--;
+    if (toLoad === 0) {
+      console.log('Textures loaded!');
+      if (typeof options.complete === 'function')
+        options.complete(textures);
+    }
+    else {
+      var pct = (options.textures.length-toLoad)/options.textures.length*100;
+
+      console.log('Loading textures: '+pct.toFixed(0)+'%');
+      if (typeof options.progress === 'function')
+        options.progress(pct, textures[name]);
+    }
+  };
+
+  options.textures.forEach(function(name, index) {
+    // Strip file extension
+    var shortName = name.split('.')[0];
+
+    new THREE.ImageUtils.loadTexture('/base/tests/mock/textures/' + name, {}, loaded.bind(null, shortName));
+  });
+};
+
 
 // Init the game one time before running the tests
 s.init();
