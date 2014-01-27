706d882131ffb275a5c888c6c9b252118b43cf28
Joao Stein <joaosteing@gmail.com>
2014-01-26 16:55:36 -0800

    refactor s.BotSpec file to use new runAsync function and move the creation of a new player and bot to a beforeEach function that runs before all tests

--- tests/client/unit/s.BotSpec.js
+++ tests/client/unit/s.BotSpec.js
@@ -1,25 +1,34 @@
 describe('Bot class', function () {
+  beforeEach(function (done) {
+    var createPlayers = function () {
+      s.game.player = makePlayer('Player one');
+      bot = makeBot();
+    };
+
+    runAsync(createPlayers, done);
+  });
+
   it('should create new Bot instance', function (done) {
-    var specs = function (bot) {
+    var specs = function () {
       expect(bot).to.be.an('object');
       expect(bot.isBot).to.equal(true);
     };
 
-    runSpecs(specs, done);
+    runAsync(specs, done);
   });
 
   it('should increment bot counter', function (done) {
-    var specs = function (bot) {
+    var specs = function () {
       // Expect 2 because the spec share the same game object
       // Previous test add the first one
       expect(s.game.botCount).to.equal(2);
     };
 
-    runSpecs(specs, done);
+    runAsync(specs, done);
   });
 
   it('should have botOptions property with default values', function (done) {
-    var specs = function (bot) {
+    var specs = function () {
       expect(bot.botOptions).to.be.an('object');
       expect(bot.botOptions).to.have.property('rotationSpeed').and.to.not.equal(undefined);
       expect(bot.botOptions).to.have.property('pitchSpeed').and.to.not.equal(undefined);
@@ -30,12 +39,12 @@ describe('Bot class', function () {
       expect(bot.botOptions).to.have.property('rotationFadeFactor').and.to.not.equal(undefined);
     };
 
-    runSpecs(specs, done);
+    runAsync(specs, done);
   });
 
   describe('handle enemies', function () {
     it('should get enemies list', function (done) {
-      var specs = function (bot) {
+      var specs = function () {
         expect(bot.getEnemyList).to.be.an('function');
         expect(bot.botEnemyList).to.equal(undefined);
 
@@ -51,11 +60,11 @@ describe('Bot class', function () {
         expect(bot.botEnemyList[1].name).to.equal('Player two');
       };
 
-      runSpecs(specs, done);
+      runAsync(specs, done);
     });
 
     it('should get closest enemy distance', function (done) {
-      var specs = function (bot) {
+      var specs = function () {
         bot.getEnemyList();
 
         expect(bot.getClosestDistance).to.be.an('function');
@@ -64,97 +73,97 @@ describe('Bot class', function () {
         expect(bot.closestDistance).to.not.equal(undefined).and.to.not.equal(null);
       };
 
-      runSpecs(specs, done);
+      runAsync(specs, done);
     });
   });
 
   describe('should control its directions and move', function () {
     describe('when enemy is in front of bot', function () {
       it('should turn left based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, -0.16, 0, 0.5);
           bot.controlBot();
           expect(bot.moveStates.yaw).to.be.above(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn right based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, 0, 0.5);
           bot.controlBot();
           expect(bot.moveStates.yaw).to.be.below(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn down based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, -0.16, 0.5);
           bot.controlBot();
           expect(bot.moveStates.pitch).to.be.below(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn up based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, 0.16, 0.5);
           bot.controlBot();
           expect(bot.moveStates.pitch).to.be.above(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
     });
 
     describe('when enemy is behind of bot', function () {
       it('should turn right based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, -0.16, 0, 1);
           bot.controlBot();
           expect(bot.moveStates.yaw).to.be.below(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn left based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, 0, 1);
           bot.controlBot();
           expect(bot.moveStates.yaw).to.be.above(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn up based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, -0.16, 1);
           bot.controlBot();
           expect(bot.moveStates.pitch).to.be.above(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should turn down based on enemy position', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeGetEnemyPositionsStub(bot, 0.16, 0.16, 1);
           bot.controlBot();
           expect(bot.moveStates.pitch).to.be.below(0);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
     });
 
     it('should get enemy list and get closest enemy distance on every call', function (done) {
-      var specs = function (bot) {
+      var specs = function () {
         var getEnemyList = sinon.spy(bot, 'getEnemyList');
         var getClosestDistance = sinon.spy(bot, 'getClosestDistance');
 
@@ -167,11 +176,11 @@ describe('Bot class', function () {
         expect(bot.closestDistance).to.not.equal(undefined).and.to.not.equal(null);
       };
 
-      runSpecs(specs, done);
+      runAsync(specs, done);
     });
 
     it('should thrust if closest enemy is far away', function (done) {
-      var specs = function (bot) {
+      var specs = function () {
         var stub = makeDistanceStub(bot, 5000);
         expect(bot.botOptions.thrustImpulse).to.equal(0);
         bot.controlBot();
@@ -179,11 +188,11 @@ describe('Bot class', function () {
         expect(bot.botOptions.thrustImpulse).to.be.above(0);
       };
 
-      runSpecs(specs, done);
+      runAsync(specs, done);
     });
 
     it('should brake if closest enemy is in the minDistance range', function (done) {
-      var specs = function (bot) {
+      var specs = function () {
         var stub = makeDistanceStub(bot, 5000);
         expect(bot.botOptions.thrustImpulse).to.equal(0);
         bot.controlBot();
@@ -196,35 +205,35 @@ describe('Bot class', function () {
         expect(bot.botOptions.thrustImpulse).to.be.at.most(thrustImpulse);
       };
 
-      runSpecs(specs, done);
+      runAsync(specs, done);
     });
 
     describe('between max and min distance', function () {
       it('should thurst', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeDistanceStub(bot, 3000);
           bot.controlBot();
           expect(bot.moveStates.thrust).to.equal(1);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
 
       it('should brake', function (done) {
-        var specs = function (bot) {
+        var specs = function () {
           var stub = makeDistanceStub(bot, 1501);
           bot.botOptions.thrustImpulse += 1;
           bot.controlBot();
           expect(bot.moveStates.brakes).to.equal(1);
         };
 
-        runSpecs(specs, done);
+        runAsync(specs, done);
       });
     });
   });
 
   it('should fire', function (done) {
-    var specs = function (bot) {
+    var specs = function () {
       var spy = sinon.spy(bot, 'fire');
       var positionStub = makeGetEnemyPositionsStub(bot, 0.15, 0.15, 0);
       var distanceStub = makeDistanceStub(bot, 2000);
@@ -232,6 +241,6 @@ describe('Bot class', function () {
       expect(spy.called).to.equal(true);
     };
 
-    runSpecs(specs, done);
+    runAsync(specs, done);
   });
 });
