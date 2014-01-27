4732dafb17f53931033570c4792630c9a5cb4371
Joao Stein <joaosteing@gmail.com>
2014-01-26 16:29:42 -0800

    add s.ShipSpec.js file with basic test structure

---
+++ tests/client/unit/s.ShipSpec.js
@@ -0,0 +1,9 @@
+describe('Ship class', function () {
+  it('should', function (done) {
+    var specs = function (bot) {
+      expect(true).to.equal(true);
+    };
+
+    runSpecs(specs, done);
+  });
+});
