959391d8ae47f849e0c6c1e6c1cf725a446a4263
Joao Stein <joaosteing@gmail.com>
2014-01-26 19:22:32 -0800

    add test to make sure s.Ship has an options object with default values

--- tests/client/unit/s.ShipSpec.js
+++ tests/client/unit/s.ShipSpec.js
@@ -1,9 +1,22 @@
 describe('Ship class', function () {
-  it('should', function (done) {
-    var specs = function (bot) {
-      expect(true).to.equal(true);
+  beforeEach(function (done) {
+    var createPlayer = function () {
+      playerShip = makePlayer('Player ship');
     };
 
-    runSpecs(specs, done);
+    runAsync(createPlayer, done);
+  });
+
+  it('should have options object with default values', function (done) {
+    var specs = function () {
+      expect(playerShip.options).to.have.property('leftTurretOffset').and.to.not.equal(undefined);
+      expect(playerShip.options).to.have.property('rightTurretOffset').and.to.not.equal(undefined);
+      expect(playerShip.options).to.have.property('missileOffset').and.to.not.equal(undefined);
+      expect(playerShip.options).to.have.property('turretFireTime').and.to.not.equal(undefined);
+      expect(playerShip.options).to.have.property('botTurretFireTime').and.to.not.equal(undefined);
+      expect(playerShip.options).to.have.property('missileFireTime').and.to.not.equal(undefined);
+    };
+
+    runAsync(specs, done);
   });
 });
