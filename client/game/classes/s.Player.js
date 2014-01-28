98644d0c0200899bb63e2cf4c2ff8f77b55882b5
ehannum <ehannum@gmail.com>
2014-01-28 10:56:53 -0800

    Crosshairs and stuff o Please enter the commit message for your changes. Lines starting

--- client/game/classes/s.Player.js
+++ client/game/classes/s.Player.js
@@ -16,19 +16,11 @@ s.Player = new Class({
 
     // Root camera to the player's position
     this.root.add( this.camera );
-    
-    this.game.hook(this.update);
 
     // Setup camera: Cockpit view; COMMENT OUT FOR CHASE CAM
     // this.camera.position.set( 0, 0, 0 );
 
     // Setup camera: Chase view
     this.game.camera.position.set(0,35,250);
-  },
-
-  update: function() {
-    if (this.hull <= 0){
-      this.game.handleDie();
-    }
   }
 });
