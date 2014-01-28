becf4828908539643f6f3e081a291e674976a6b6
steinernein <steinernein@gmail.com>
2014-01-28 00:32:06 -0800

    "Serious issue involving double join and now rooms are hard coded =( "

--- server/io/rooms.js
+++ server/io/rooms.js
@@ -12,11 +12,11 @@ Room.prototype.init = function( socket, room, playerData ) {
   this.rooms[room].gamestate = {};
   this.rooms[room].playerList = {};
   this.rooms[room].playerList[playerData.name] = playerData;
-  this.sockets[socket] = {name: playerData.name, socket: socket, ship: playerData.ship, room: room};
+  this.sockets[socket.id] = {name: playerData.name, socket: socket.id, ship: playerData.ship, room: room};
 };
 
 Room.prototype.add = function( socket, room, playerData) {
-  this.sockets[socket] = {name: playerData.name, room: room, ship: playerData.ship};
+  this.sockets[socket.id] = {name: playerData.name, room: room, ship: playerData.ship};
   this.rooms[room].playerList[playerData.name] = playerData;
 };
 
