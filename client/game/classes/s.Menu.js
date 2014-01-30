b9cdd05e057361ddfc4e0d7b0079fe5a5e3c4a03
ehannum <ehannum@gmail.com>
2014-01-29 17:47:52 -0800

    Adding players vs bots/players vs players toggle to create game menu

--- client/game/classes/s.Menu.js
+++ client/game/classes/s.Menu.js
@@ -277,7 +277,6 @@ s.Menu = new Class({
     this.game.roomEntered = true;
     room = this.hoveredItem.theRoomYouWillJoin;
     // room = 'asdf';
-    this.game.room = room;
     this.game.comm.connectSockets();
     this.close();
   },
@@ -287,14 +286,30 @@ s.Menu = new Class({
     var prefix = this.roomNamePrefix[Math.floor(Math.random()*this.roomNamePrefix.length)];
     var suffix = this.roomNameSuffix[Math.floor(Math.random()*this.roomNameSuffix.length)];
 
-    var roomName = prefix.toUpperCase() + '-' + suffix.toUpperCase();
+    this.game.room = this.game.room || prefix.toUpperCase() + '-' + suffix.toUpperCase();
+    var bots = this.game.humansOnly ? 'OFF' : 'ON';
 
     this.addMenuItems([
-      {text: 'NAME: '+roomName, size: 5},
-      {text: 'CHANGE NAME', size: 4, action: 'showCreateRoom'}, // todo: make an updateMenuItem function
+      {text: 'NAME: '+this.game.room.toUpperCase(), size: 5},
+      {text: 'CHANGE NAME', size: 4, action: 'shuffleRoom'},
+      {text: 'BOTS: '+bots, size: 4, action: 'toggleBots'},
       {text: 'INVASION MODE', size: 5, action: 'joinRoom', room: roomName},
       {text: 'FREE-FOR-ALL', size: 5, action: 'joinRoom', room: roomName}
     ]);
+  },
+
+  // todo: make an updateMenuItem function!
+  shuffleRoom: function () {
+    var prefix = this.roomNamePrefix[Math.floor(Math.random()*this.roomNamePrefix.length)];
+    var suffix = this.roomNameSuffix[Math.floor(Math.random()*this.roomNameSuffix.length)];
+
+    this.game.room = prefix.toUpperCase() + '-' + suffix.toUpperCase();
+    this.showCreateRoom();
+  },
+
+  toggleBots: function () {
+    this.game.humansOnly = !this.game.humansOnly;
+    this.showCreateRoom();
   },
 
   showScoreboard: function () {
