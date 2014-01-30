f62ec604d1f6c969bdc1ce48f2d033df56e86c22
Sam Stites <stites.sr@gmail.com>
2014-01-30 14:55:52 -0800

    joining a room in-game makes you leave the previous one

--- server/events/player.js
+++ server/events/player.js
@@ -34,7 +34,6 @@ module.exports = function (host, sync, io) {
     },
 
     botInfo: function ( socket, packet) {
-      console.log(host.sockets[socket.id]);
       var room = host.sockets[socket.id].room;
       var target = host.rooms[room].bot;
       if(target === undefined || room === undefined) {
